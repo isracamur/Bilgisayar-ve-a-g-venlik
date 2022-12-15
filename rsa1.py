2569fa6207fcf56386526630b9711437ddf0d1e9
isracamur <97159365+isracamur@users.noreply.github.com>
2022-12-15 17:58:08 +0300

    Add files via upload

---
+++ rsa1.py
@@ -0,0 +1,18 @@
+from Crypto.Util.number import getPrime, long_to_bytes, bytes_to_long
+
+p = getPrime(256)
+q = getPrime(256)
+p=11
+q=13
+n = p * q
+print()
+tot = (p - 1) * (q - 1)
+
+e = 65537
+d = pow(e, -1, tot)
+
+print("n:", n)
+print("e:", e)
+print("d:", d)
+
+
